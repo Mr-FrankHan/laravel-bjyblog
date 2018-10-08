23f94e44850bf5cfa203dbb5925292af87a59bf1
Mr-FrankHan <frankhan1992@163.com>
2018-10-08 15:03:02 +0800

    添加百度自动推送链接代码

--- resources/views/home/index/article.blade.php
+++ resources/views/home/index/article.blade.php
@@ -158,4 +158,19 @@
     <script type="text/javascript">
         $('.js-content img').zoomify()
     </script>
+    {{--百度自动推送链接代码--}}
+    <script>
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+    </script>
 @endsection
