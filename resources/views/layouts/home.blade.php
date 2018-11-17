64c46a47e23bceab1aeaf740267fd836a6921fbd
Mr-FrankHan <frankhan1992@163.com>
2018-11-17 09:38:49 +0800

    去除无用字符串

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -22,7 +22,7 @@
         <div class="navbar-header" style="margin-right: 40px">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                     data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
